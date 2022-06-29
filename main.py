b9861bb3ec1d5ba771397b95379448554c7a2491
Felix Fennell <felnne@bas.ac.uk>
2022-06-29 19:05:42 +0100

    Adding persistence using GeoPackage
    Adds support for dumping and loading routes and waypoints to a GeoPackage.
    
    In order to load items back, all classes have changed to use properties (changing their init methods) and added methods for dumping and loading from Fiona/(Py)geo_interface features.
    
    Currently the network manager class is responsible for dumping/loading GeoPackages as complete, multi-layer, objects. That’s fine for GeoPackages but for export formats we’ll want to be able to dump just routes or just waypoints etc. Once that happens the GeoPackage dump/load functions will be refactored to match.
    
    The test network manager, used for loading raw test data as part of initial development, has been updated oto match the changes to the main classes (notably needing to lookup Waypoints by designator itself).

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from bas_air_unit_waypoints import (
     Route,
     WaypointCollection,
     NetworkManager,
+    RouteWaypoint,
 )
 
 
@@ -14,16 +15,15 @@ class TestNetworkManager(NetworkManager):
     def __init__(self, data_path: Path):
         super().__init__()
         self.data_path: Path = data_path
-        self.load_test_data()
 
     def load_test_data(self) -> None:
         with open(self.data_path, mode="r") as test_data_file:
             raw_test_data: dict = json.load(test_data_file)
 
-        self.load_test_waypoints(raw_waypoints=raw_test_data["waypoints"])
-        self.load_test_routes(raw_routes=raw_test_data["routes"], waypoints=self.waypoints)
+        self._load_test_waypoints(raw_waypoints=raw_test_data["waypoints"])
+        self._load_test_routes(raw_routes=raw_test_data["routes"], waypoints=self.waypoints)
 
-    def load_test_waypoints(self, raw_waypoints: List[dict]) -> None:
+    def _load_test_waypoints(self, raw_waypoints: List[dict]) -> None:
         for raw_waypoint in raw_waypoints:
             self.waypoints.append(
                 Waypoint(
@@ -31,18 +31,40 @@ class TestNetworkManager(NetworkManager):
                     lon=raw_waypoint["lon"],
                     lat=raw_waypoint["lat"],
                     comment=raw_waypoint["comment"],
+                    last_accessed_at=raw_waypoint["last_accessed_at"],
+                    last_accessed_by=raw_waypoint["last_accessed_by"],
                 )
             )
 
-    def load_test_routes(self, raw_routes: List[dict], waypoints: WaypointCollection):
+    def _load_test_routes(self, raw_routes: List[dict], waypoints: WaypointCollection):
         for raw_route in raw_routes:
-            self.routes.append(
-                Route(
-                    name=raw_route["name"],
-                    route_waypoints=raw_route["waypoints"],
-                    waypoints=waypoints,
-                )
-            )
+            route = Route(name=raw_route["name"])
+            waypoints: List[RouteWaypoint] = []
+
+            sequence = 1
+            for waypoint in raw_route["waypoints"]:
+                route_waypoint = RouteWaypoint()
+
+                # find waypoint by designator
+                for _waypoint in self.waypoints:
+                    if _waypoint.designator == waypoint["waypoint_designator"]:
+                        route_waypoint.waypoint = _waypoint
+                if route_waypoint.waypoint is None:
+                    raise RuntimeError(
+                        f"Unable to find Waypoint with designator '[{waypoint['waypoint_designator']}]' in Waypoints list."
+                    )
+
+                route_waypoint.sequence = sequence
+
+                if "description" in waypoint:
+                    route_waypoint.description = waypoint["description"]
+
+                waypoints.append(route_waypoint)
+                sequence += 1
+
+            route.waypoints = waypoints
+
+            self.routes.append(route)
 
     def describe(self) -> None:
         print(f"Waypoints in test WaypointCollection [{self.waypoints.count}]:")
@@ -58,10 +80,18 @@ class TestNetworkManager(NetworkManager):
 
 
 test_data_path = Path("test-data-raw.json")
-
+test_dataset_path = Path("test-dataset.gpkg")
 
 if __name__ == "__main__":
     test_network = TestNetworkManager(data_path=test_data_path)
+
+    # test_network.load_test_data()
+    # print(test_network)
+    # print("")
+    # test_network.describe()
+    # test_network.dump_gpkg(path=test_dataset_path)
+
+    test_network.load_gpkg(path=test_dataset_path)
     print(test_network)
     print("")
     test_network.describe()
--- src/bas_air_unit_waypoints/__init__.py
+++ src/bas_air_unit_waypoints/__init__.py
@@ -1,126 +1,314 @@
-from datetime import datetime
-from typing import Optional, List, Dict, Iterator
+from datetime import date
+from pathlib import Path
+from typing import Optional, List, Dict, Iterator, Union
 
-from shapely.geometry import Point
+import fiona
 import ulid
+from fiona.crs import from_epsg as crs_from_epsg
+from shapely.geometry import Point
 
 
 class Waypoint:
     designator_max_length = 6
 
+    feature_schema = {
+        "geometry": "Point",
+        "properties": {
+            "id": "str",
+            "designator": "str",
+            "comment": "str",
+            "last_accessed_at": "date",
+            "last_accessed_by": "str",
+        },
+    }
+
     def __init__(
         self,
-        designator: str,
-        lon: float,
-        lat: float,
+        designator: Optional[str] = None,
+        lon: Optional[float] = None,
+        lat: Optional[float] = None,
         alt: Optional[float] = None,
         comment: Optional[str] = None,
-        last_accessed_at: Optional[datetime] = None,
+        last_accessed_at: Optional[date] = None,
         last_accessed_by: Optional[str] = None,
     ) -> None:
-        self.id: str = self._make_id()
-        self.designator: str = designator
-        self.geometry: Point = Point(lon, lat)
-        self.comment: str = comment
-        self.last_accessed_at: datetime = last_accessed_at
-        self.last_accessed_by: str = last_accessed_by
+        self._id: str = str(ulid.new())
 
-        if len(designator) > Waypoint.designator_max_length:
-            raise ValueError(f"Designators must be 6 characters or less. '{designator}' is {len(designator)}.")
+        self._designator: str
+        self._geometry: Point
+        self._comment: Optional[str] = None
+        self._last_accessed_at: Optional[date] = None
+        self._last_accessed_by: Optional[str] = None
 
-        if lon < -180 or lon > 180:
-            raise ValueError(f"Invalid Longitude, must be -180<=X<=180 not {lon}.")
-        if lat < -90 or lat > 90:
-            raise ValueError(f"Invalid Latitude, must be -90<=Y<=+90 not {lat}.")
+        if designator is not None:
+            self.designator = designator
 
-        if alt is not None:
-            self.geometry = Point(lon, lat, alt)
+        _geometry = []
+        if lat is None and lon is not None:
+            raise ValueError("A latitude (`lat`) value must be provided if longitude (`lon`) is set.")
+        if lat is not None and lon is None:
+            raise ValueError("A longitude (`lon`) value must be provided if latitude (`lat`) is set.")
+        elif lat is not None and lon is not None and alt is None:
+            _geometry = [lon, lat]
+        elif lat is not None and lon is not None and alt is not None:
+            _geometry = [lon, lat, alt]
+        if len(_geometry) >= 2:
+            self.geometry = _geometry
+
+        if comment is not None:
+            self.comment = comment
 
         if last_accessed_at is not None and last_accessed_by is None:
             raise ValueError("A `last_accessed_by` value must be provided if `last_accessed_at` is set.")
-        if last_accessed_at is None and last_accessed_by is not None:
+        elif last_accessed_at is None and last_accessed_by is not None:
             raise ValueError("A `last_accessed_at` value must be provided if `last_accessed_by` is set.")
+        elif last_accessed_at is not None and last_accessed_by is not None:
+            self.last_accessed_at = last_accessed_at
+            self.last_accessed_by = last_accessed_by
 
-    @staticmethod
-    def _make_id() -> str:
-        return str(ulid.new())
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @id.setter
+    def id(self, _id):
+        self._id = ulid.from_str(_id)
+
+    @property
+    def designator(self) -> str:
+        return self._designator
+
+    @designator.setter
+    def designator(self, designator: str):
+        if len(designator) > Waypoint.designator_max_length:
+            raise ValueError(f"Designators must be 6 characters or less. '{designator}' is {len(designator)}.")
+
+        self._designator = designator
+
+    @property
+    def geometry(self) -> Point:
+        return self._geometry
+
+    @geometry.setter
+    def geometry(self, geometry: List[float]):
+        lon = geometry[0]
+        if lon < -180 or lon > 180:
+            raise ValueError(f"Invalid Longitude, must be -180<=X<=180 not {lon}.")
+        lat = geometry[1]
+        if lat < -90 or lat > 90:
+            raise ValueError(f"Invalid Latitude, must be -90<=Y<=+90 not {lat}.")
+        self._geometry = Point(lon, lat)
+
+        try:
+            alt = geometry[2]
+            self._geometry = Point(lon, lat, alt)
+        except IndexError:
+            pass
+
+    @property
+    def comment(self) -> Optional[str]:
+        return self._comment
+
+    @comment.setter
+    def comment(self, comment: str):
+        self._comment = comment
+
+    @property
+    def last_accessed_at(self) -> Optional[date]:
+        return self._last_accessed_at
+
+    @last_accessed_at.setter
+    def last_accessed_at(self, _date: date):
+        self._last_accessed_at = _date
+
+    @property
+    def last_accessed_by(self) -> Optional[str]:
+        return self._last_accessed_by
+
+    @last_accessed_by.setter
+    def last_accessed_by(self, last_accessed_by: str):
+        self._last_accessed_by = last_accessed_by
+
+    def loads_feature(self, feature: dict):
+        self.id = feature["properties"]["id"]
+        self.designator = feature["properties"]["designator"]
+        self.geometry = list(feature["geometry"]["coordinates"])
+
+        if feature["properties"]["comment"] is not None:
+            self.comment = feature["properties"]["comment"]
+
+        if feature["properties"]["last_accessed_at"] is not None and feature["properties"]["last_accessed_by"] is None:
+            raise ValueError("A `last_accessed_by` value must be provided if `last_accessed_at` is set.")
+        elif (
+            feature["properties"]["last_accessed_at"] is None and feature["properties"]["last_accessed_by"] is not None
+        ):
+            raise ValueError("A `last_accessed_at` value must be provided if `last_accessed_by` is set.")
+        elif (
+            feature["properties"]["last_accessed_at"] is not None
+            and feature["properties"]["last_accessed_by"] is not None
+        ):
+            self.last_accessed_at = date.fromisoformat(feature["properties"]["last_accessed_at"])
+            self.last_accessed_by = feature["properties"]["last_accessed_by"]
+
+    def dumps_feature(self) -> dict:
+        _geometry = {"type": "Point", "coordinates": (self.geometry.x, self.geometry.y)}
+        if self.geometry.has_z:
+            _geometry["coordinates"] = (self.geometry.x, self.geometry.y, self.geometry.z)
+
+        return {
+            "geometry": _geometry,
+            "properties": {
+                "id": self.id,
+                "designator": self.designator,
+                "comment": self.comment,
+                "last_accessed_at": self.last_accessed_at,
+                "last_accessed_by": self.last_accessed_by,
+            },
+        }
 
     def __repr__(self) -> str:
         return f"<Waypoint {self.id} :- [{self.designator.ljust(6, '_')}], {self.geometry}>"
 
 
 class RouteWaypoint:
-    def __init__(self, waypoint: Waypoint, sequence: int, description: Optional[str] = None) -> None:
-        self.waypoint: Waypoint = waypoint
-        self.description: str = description
-        self.sequence: int = sequence
+    def __init__(
+        self, waypoint: Optional[Waypoint] = None, sequence: Optional[int] = None, description: Optional[str] = None
+    ) -> None:
+        self._waypoint: Waypoint
+        self._sequence: int
+        self._description: Optional[str] = None
+
+        if waypoint is not None and sequence is None:
+            raise ValueError("A `sequence` value must be provided if `waypoint` is set.")
+        elif waypoint is None and sequence is not None:
+            raise ValueError("A `waypoint` value must be provided if `sequence` is set.")
+        elif waypoint is not None and sequence is not None:
+            self.waypoint = waypoint
+            self.sequence = sequence
+
+        if description is not None:
+            self.description = description
+
+    @property
+    def waypoint(self) -> Waypoint:
+        return self._waypoint
+
+    @waypoint.setter
+    def waypoint(self, waypoint: Waypoint):
+        self._waypoint = waypoint
+
+    @property
+    def sequence(self) -> int:
+        return self._sequence
+
+    @sequence.setter
+    def sequence(self, sequence: int):
+        self._sequence = sequence
+
+    @property
+    def description(self) -> str:
+        return self._description
+
+    @description.setter
+    def description(self, description: str):
+        self._description = description
+
+    def loads_feature(self, feature: dict, waypoints: "WaypointCollection"):
+        self.sequence = feature["properties"]["sequence"]
+        self.description = feature["properties"]["description"]
+
+        try:
+            self.waypoint = waypoints[feature["properties"]["waypoint_id"]]
+        except KeyError:
+            raise KeyError(
+                f"Waypoint with ID '{feature['properties']['waypoint_id']}' not found in available waypoints."
+            )
 
 
 class Route:
+    feature_schema = {
+        "geometry": "None",
+        "properties": {"id": "str", "name": "str"},
+    }
+
     def __init__(
         self,
-        name: str,
-        route_waypoints: List[Dict[str, str]],
-        waypoints: "WaypointCollection",
+        name: Optional[str] = None,
+        route_waypoints: Optional[List[Dict[str, Union[str, Waypoint]]]] = None,
     ) -> None:
-        self.id: str = self._make_id()
-        self.name: str = name
-        self.waypoints: List[RouteWaypoint] = self._process_waypoints(
-            route_waypoints=route_waypoints, waypoints=waypoints
-        )
+        self._id: str = str(ulid.new())
 
-    @staticmethod
-    def _make_id() -> str:
-        return str(ulid.new())
+        self._name: str
+        self._waypoints: List[RouteWaypoint] = []
 
-    @staticmethod
-    def _process_waypoints(
-        route_waypoints: List[Dict[str, str]], waypoints: "WaypointCollection"
-    ) -> List[RouteWaypoint]:
-        sequence = 1
-        _waypoints: List[RouteWaypoint] = []
+        if name is not None:
+            self.name = name
 
-        for route_waypoint in route_waypoints:
-            try:
-                waypoint = waypoints.get_by_designator(designator=route_waypoint["waypoint_designator"])
-            except IndexError:
-                raise IndexError(
-                    f"Waypoint with designator '{route_waypoint['waypoint_designator']}' not found in available waypoints."
-                )
-
-            try:
-                _description = route_waypoint["description"]
-            except KeyError:
-                _description = None
-
-            _waypoints.append(
-                RouteWaypoint(
-                    waypoint=waypoint,
-                    sequence=sequence,
-                    description=_description,
-                )
-            )
-            sequence += 1
-
-        return _waypoints
+        if route_waypoints is not None:
+            self.waypoints = route_waypoints
 
     @property
-    def first_waypoint(self) -> RouteWaypoint:
-        return self.waypoints[0]
+    def id(self) -> str:
+        return self._id
+
+    @id.setter
+    def id(self, _id: str):
+        self._id = ulid.from_str(_id)
 
     @property
-    def last_waypoint(self) -> RouteWaypoint:
-        return self.waypoints[-1]
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, name: str):
+        self._name = name
+
+    @property
+    def waypoints(self) -> List[RouteWaypoint]:
+        return self._waypoints
+
+    @waypoints.setter
+    def waypoints(self, route_waypoints: List[RouteWaypoint]):
+        self._waypoints = route_waypoints
+
+    @property
+    def first_waypoint(self) -> Optional[RouteWaypoint]:
+        try:
+            return self.waypoints[0]
+        except IndexError:
+            return None
+
+    @property
+    def last_waypoint(self) -> Optional[RouteWaypoint]:
+        try:
+            return self.waypoints[-1]
+        except IndexError:
+            return None
 
     @property
     def waypoints_count(self) -> int:
         return len(self.waypoints)
 
+    def loads_feature(self, feature: dict):
+        self.id = feature["properties"]["id"]
+        self.name = feature["properties"]["name"]
+
+    def dumps_feature(self) -> dict:
+        return {
+            "properties": {"id": self.id, "name": self.name},
+        }
+
     def __repr__(self) -> str:
-        return (
-            f"<Route {self.id} :- [{self.name.ljust(10, '_')}], {self.waypoints_count} waypoints, Start/End: "
-            f"{self.first_waypoint.waypoint.designator.ljust(6)} / {self.last_waypoint.waypoint.designator.ljust(6)}>"
-        )
+        _start = "-"
+        _end = "-"
+
+        try:
+            _start = self.first_waypoint.waypoint.designator.ljust(6)
+            _end = self.last_waypoint.waypoint.designator.ljust(6)
+        except AttributeError:
+            pass
+
+        return f"<Route {self.id} :- [{self.name.ljust(10, '_')}], {self.waypoints_count} waypoints, Start/End: {_start} / {_end}>"
 
 
 class WaypointCollection:
@@ -134,14 +322,14 @@ class WaypointCollection:
     def count(self) -> int:
         return len(self._waypoints)
 
-    def get_by_designator(self, designator: str) -> Waypoint:
+    def __getitem__(self, _id: str) -> Waypoint:
         for waypoint in self._waypoints:
-            if waypoint.designator == designator:
+            if waypoint.id == _id:
                 return waypoint
 
-        raise IndexError(f"Waypoint with designator '{designator}' not found.")
+        raise KeyError(_id)
 
-    def __iter__(self) -> Iterator:
+    def __iter__(self) -> Iterator[Waypoint]:
         return self._waypoints.__iter__()
 
     def __repr__(self) -> str:
@@ -159,7 +347,14 @@ class RouteCollection:
     def count(self) -> int:
         return len(self._routes)
 
-    def __iter__(self) -> Iterator:
+    def __getitem__(self, _id: str) -> Route:
+        for route in self._routes:
+            if route.id == _id:
+                return route
+
+        raise KeyError(_id)
+
+    def __iter__(self) -> Iterator[Route]:
         return self._routes.__iter__()
 
     def __repr__(self) -> str:
@@ -172,6 +367,96 @@ class NetworkManager:
     def __init__(self):
         self.waypoints: WaypointCollection = WaypointCollection()
         self.routes: RouteCollection = RouteCollection()
+
+    def load_gpkg(self, path: Path):
+        # waypoints
+        with fiona.open(path, mode="r", driver="GPKG", layer="waypoints") as layer:
+            for waypoint_feature in layer:
+                _waypoint = Waypoint()
+                _waypoint.loads_feature(feature=waypoint_feature)
+                self.waypoints.append(_waypoint)
+
+        # routes & route-waypoints
+        with fiona.open(path, mode="r", driver="GPKG", layer="routes") as layer:
+            for route_feature in layer:
+                _route = Route()
+                _route.loads_feature(feature=route_feature)
+                self.routes.append(_route)
+        with fiona.open(path, mode="r", driver="GPKG", layer="route_waypoints") as layer:
+            # process route waypoints and group by route
+            route_waypoints_by_route_id: Dict[str, List[RouteWaypoint]] = {}
+            for route_waypoint_feature in layer:
+                _route_waypoint = RouteWaypoint()
+                _route_waypoint_feature = {"sequence": route_waypoint_feature["properties"]["sequence"]}
+
+                _route_waypoint.loads_feature(feature=route_waypoint_feature, waypoints=self.waypoints)
+
+                if route_waypoint_feature["properties"]["route_id"] not in route_waypoints_by_route_id.keys():
+                    route_waypoints_by_route_id[route_waypoint_feature["properties"]["route_id"]] = []
+                route_waypoints_by_route_id[route_waypoint_feature["properties"]["route_id"]].append(_route_waypoint)
+
+            for route_id, route_waypoint_features in route_waypoints_by_route_id.items():
+                route = self.routes[route_id]
+                route.waypoints = route_waypoint_features
+
+    def dump_gpkg(self, path: Path):
+        # waypoints
+        waypoint_schema = {
+            "geometry": "Point",
+            "properties": {
+                "id": "str",
+                "designator": "str",
+                "comment": "str",
+                "last_accessed_at": "date",
+                "last_accessed_by": "str",
+            },
+        }
+        with fiona.open(
+            path, mode="w", driver="GPKG", crs=crs_from_epsg(4326), schema=waypoint_schema, layer="waypoints"
+        ) as layer:
+            for waypoint in self.waypoints:
+                layer.write(waypoint.dumps_feature())
+
+                layer.write(
+                    {
+                        "geometry": {"type": "Point", "coordinates": (waypoint.geometry.x, waypoint.geometry.y)},
+                        "properties": {
+                            "id": waypoint.id,
+                            "designator": waypoint.designator,
+                            "comment": waypoint.comment,
+                            "last_accessed_at": waypoint.last_accessed_at,
+                            "last_accessed_by": waypoint.last_accessed_by,
+                        },
+                    }
+                )
+
+        # route_waypoints
+        route_waypoint_schema = {
+            "geometry": "None",
+            "properties": {"route_id": "str", "waypoint_id": "str", "description": "str", "sequence": "int"},
+        }
+        with fiona.open(path, mode="w", driver="GPKG", schema=route_waypoint_schema, layer="route_waypoints") as layer:
+            for route in self.routes:
+                for route_waypoint in route.waypoints:
+                    layer.write(
+                        {
+                            "properties": {
+                                "route_id": route.id,
+                                "sequence": route_waypoint.sequence,
+                                "waypoint_id": route_waypoint.waypoint.id,
+                                "description": route_waypoint.description,
+                            }
+                        }
+                    )
+
+        # routes
+        # (only name and any other top/route level information is stored here, waypoints are stored in `route_waypoints`)
+        route_schema = {"geometry": "None", "properties": {"id": "str", "name": "str"}}
+        with fiona.open(
+            path, mode="w", driver="GPKG", crs=crs_from_epsg(4326), schema=route_schema, layer="routes"
+        ) as layer:
+            for route in self.routes:
+                layer.write({"properties": {"id": route.id, "name": route.name}})
 
     def __repr__(self):
         return f"<NetworkManager : {self.waypoints.count} Waypoints - {self.routes.count} Routes>"
